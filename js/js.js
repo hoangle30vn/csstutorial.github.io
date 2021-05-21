66ffdef009060a568599be8316f072b7b116f52a
hoangle30 <hoanglevi199@gmail.com>
2021-05-22 02:15:43 +0700

    message nhieu nguoi

--- js/js.js
+++ js/js.js
@@ -1,12 +1,22 @@
 var submitMess = document.getElementById("submitMess");
 var nameMess = document.getElementById("nameMess");
 var contentMess = document.getElementById("contentMess");
+var numberMess = 0;
 console.log("chaohoang");
+var dataRef = firebase.database().ref("/");
+dataRef.child('num').on('value', async function(snap) {
+    var num = await snap.val();
+    numberMess = num;
+    console.log(num);
+    submit1();
+});
 
-function submit() {
-    console.log(nameMess.value + " đã gửi 1 tin nhắn: " + contentMess.value);
-    var noidung = nameMess.value.toString() + " đã gửi 1 tin nhắn: " + contentMess.value.toString();
-    var name = nameMess.value;
+function submit1() {
+    console.log(nameMess.value + " said: " + contentMess.value);
+    var noidung = nameMess.value.toString() + " said: " + contentMess.value.toString();
+    var name = nameMess.value.toString() + numberMess.toString() + "/";
+
+
     if (contentMess.value.length == 0) {
         alert("Bạn không có gì để nói với mình à");
     } else if (nameMess.value.length == 0) {
@@ -14,9 +24,18 @@ function submit() {
     } else {
         alert("Hoàng sẽ phản hồi bạn sớm nhất có thể. Chúc bạn một ngày tốt lành");
     }
-    firebase.database().ref("/").update({
-        name: "hello",
-        hoang: noidung
+    console.log(name);
+
+    firebase.database().ref(name).update({
+        mess: noidung
     });
     console.log("xong");
+
+}
+
+function submit() {
+    numberMess++;
+    firebase.database().ref("/").update({
+        num: numberMess
+    });
 }
