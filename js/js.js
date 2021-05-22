1945aa974e9d82585fbd7b77e457f22fd657f564
hoangle30 <hoanglevi199@gmail.com>
2021-05-22 17:11:37 +0700

    dem so tin nhan, luu tru theo thu tu

--- js/js.js
+++ js/js.js
@@ -4,17 +4,38 @@ var contentMess = document.getElementById("contentMess");
 var numberMess = 0;
 console.log("chaohoang");
 var dataRef = firebase.database().ref("/");
+var messsage = document.getElementById("messsage");
+// var dbRef = firebase.database().ref("/");
 dataRef.child('num').on('value', async function(snap) {
+    // number tin nhan thay doi thi thuc hien cac ham
     var num = await snap.val();
     numberMess = num;
-    console.log(num);
+    console.log("số tin nhắn" + num);
     submit1();
+    printMess();
 });
 
+function printMess() {
+    // print ra toan bo tin nhan
+    for (let index = 0; index <= numberMess; index++) {
+        dataRef.child(index).child("mess").get().then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log("tin:" + snapshot.val());
+                messsage.innerText = "tin:" + snapshot.val();
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+}
+
 function submit1() {
+    // gui tin nhan len firebase
     console.log(nameMess.value + " said: " + contentMess.value);
     var noidung = nameMess.value.toString() + " said: " + contentMess.value.toString();
-    var name = nameMess.value.toString() + numberMess.toString() + "/";
+    var name = numberMess.toString() + "/";
 
 
     if (contentMess.value.length == 0) {
@@ -34,6 +55,7 @@ function submit1() {
 }
 
 function submit() {
+    // khi gui tin nhan thi cap nhat so luong tin nhan
     numberMess++;
     firebase.database().ref("/").update({
         num: numberMess
